7231eb87a66bc886b7b7806f4ba64c9a95ccd073
cc <votaryofturms@icloud.com>
2021-12-17 14:58:52 -0500

    renders article author, headline, body , summary test

--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event';
 import MutationObserver from 'mutationobserver-shim';
 
 import Article from './Article';
 
 const dummyData = {
-    headline: 'test',
-    author: 'test',
-    summary: 'test',
-    body: 'test'
+    headline: 'headline',
+    author: 'author',
+    summary: 'summary',
+    body: 'body'
 }
 
 test('renders component without errors', () => {
     render(<Article article={dummyData} />)
 });
 
-// test('renders headline, author from the article when passed in through props', ()=> {
-// });
+test('renders headline, author from the article when passed in through props', () => {
+    render(<Article article={dummyData} />)
+    const headline = screen.queryByText(/headline/)
+    const author = screen.queryByText(/author/)
+    const summary = screen.queryByText(/summary/)
+    const body = screen.queryByText(/body/)
+    expect(headline).toBeInTheDocument()
+    expect(author).toBeInTheDocument()
+    expect(summary).toBeInTheDocument()
+    expect(body).toBeInTheDocument()
+});
 
 // test('renders "Associated Press" when no author is given', ()=> {
 // });
